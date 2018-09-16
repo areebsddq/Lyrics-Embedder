7ab1486c35153cf8acf6a406032af1c6a426ee3b
Areeb Siddiqui <areebsddq@gmail.com>
2018-09-15 23:23:45 -0400

    clean up code and fix issue with import

--- Mp3Lyrics.py
+++ Mp3Lyrics.py
@@ -1,13 +1,13 @@
 from PyQt5.QtWidgets import *
 from PyQt5 import QtCore
 
-#For fetching and manipulating the mp3 files
+# For fetching and manipulating the mp3 files
 from tkinter import *
 from tkinter.filedialog import askopenfilenames
 from mutagen.id3 import ID3NoHeaderError
-from mutagen.id3 import ID3, TIT2, TALB, TPE1, TPE2, COMM, USLT, TCOM, TCON, TDRC
+from mutagen.id3 import ID3, USLT
 
-#To scrape the webpages for lyrics
+# To scrape the webpages for lyrics
 import urllib.request
 import urllib.error
 import re
@@ -15,28 +15,34 @@ import sys
 import threading
 import ntpath
 
-#For decoding the lyrics to unicode
+# For decoding the lyrics to unicode
 import chardet
 import ftfy
 import unicodedata
+
 
 def remove_accent_letters(str):
     nfkd_form = unicodedata.normalize('NFKD', str)
     return u"".join([c for c in nfkd_form if not unicodedata.combining(c)])
 
 
-def strip_tag_details(str):
-    for c in (",", "'", "'", "`", "’", "(live)", "(Live)", "(cover)", "(Cover)" "(", ")", "?", ".",  "!", "*"):
-        str = str.replace(c, "")
+def remove_brackets(str):
+    return re.sub(r"\(.+\)", "", str)
 
-    str = str.replace("&", "and")
-    str = str.replace("/", " ")
-    str = remove_accent_letters(str)
 
-    return str
+def strip_tag_details(word):
+    for c in (",", "'", "'", "`", "’", "?", ".", "!", "*"):
+        word = word.replace(c, "")
 
-def get_lyrics(title, artist):
-    #Incase there are several artists
+    word = word.replace("&", "and")
+    word = word.replace("/", " ")
+    word = remove_accent_letters(word)
+
+    return word
+
+
+def create_url(title, artist):
+    # In case there are several artists
     artist = artist.split(",")[0]
 
     title = strip_tag_details(title)
@@ -44,49 +50,62 @@ def get_lyrics(title, artist):
 
     wordList = artist.split() + title.split()
     wordList.append("lyrics")
-    
+
     urlTrail = '-'.join(wordList)
 
     url = "https://genius.com/" + urlTrail
 
-    try:
-        #We give a known user agent so that the site doesn't block access.
-        reqObj = urllib.request.Request(url,headers={'User-Agent': 'Mozilla/5.0'})
-        page = urllib.request.urlopen(reqObj)
+    return url
 
-    except:
-        print("Error with url (may not exist): " + url, file = sys.stderr)
-        return
 
-    pageData = page.read()
-
-    charDict = chardet.detect(pageData)
-    encType = charDict['encoding']
-
-    decodeFail = False
+def get_lyrics(title, artist):
+    url = create_url(title, artist)
 
     try:
-        pageData.decode(encType)
+        # We give a known user agent so that the site doesn't block access.
+        req_obj = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+        page = urllib.request.urlopen(req_obj)
 
     except:
-        print("Error decoding data for: " + title, file = sys.stderr)
-        decodeFail = True
+        try:
+            url = create_url(remove_brackets(title), artist)
 
-    #Find lyrics in html data with a regex
-    lyricsList = re.findall(r'<div class="lyrics">.*?</div>', str(pageData))
+            req_obj = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+            page = urllib.request.urlopen(req_obj)
 
-    if not len(lyricsList) == 1:
-        print("Error scraping for lyrics for:" + title, file = sys.stderr)
+        except:
+            print("Error with url (may not exist): " + url, file=sys.stderr)
+            return
+
+    page_data = page.read()
+
+    char_dict = chardet.detect(page_data)
+    enc_type = char_dict['encoding']
+
+    decode_fail = False
+
+    try:
+        page_data.decode(enc_type)
+
+    except:
+        print("Error decoding data for: " + title, file=sys.stderr)
+        decode_fail = True
+
+    # Find lyrics in html data
+    lyrics_list = re.findall(r'<div class="lyrics">.*?</div>', str(page_data))
+
+    if not len(lyrics_list) == 1:
+        print("Error scraping for lyrics for:" + title, file=sys.stderr)
         return
 
-    lyrics = str(lyricsList[0])
+    lyrics = str(lyrics_list[0])
 
-    #Scraper escapes \n and other chars automatically so we have to undo that.
+    # Scraper escapes \n and other chars automatically so we have to undo that.
     lyrics = lyrics.encode().decode('unicode_escape')
 
-    #Was getting error here in a weird edge case
+    # Was getting error here in a weird edge case
     try:
-        #Remove extra html junk
+        # Remove extra html junk
         lyrics = lyrics.replace("<br>",'')
         lyrics = lyrics.split("<p>")[1]
         lyrics = lyrics.split("</p>")[0]
@@ -94,18 +113,18 @@ def get_lyrics(title, artist):
     except:
         pass
 
-    htmlJunk = re.findall(r"<[\s\S]*?>", str(lyrics))
+    html_junk = re.findall(r"<[\s\S]*?>", str(lyrics))
 
-    for junk in htmlJunk:
+    for junk in html_junk:
         lyrics = lyrics.replace(junk, "")
 
-    #To fix random unicode text issues that may pop up
+    # To fix random unicode text issues that may pop up
     lyrics = ftfy.fix_text(lyrics)
 
-    #Some edge cases that fail decoding work with this:
-    if decodeFail:
+    # Some cases that fail decoding work with this instead:
+    if decode_fail:
         try:
-            lyrics = lyrics.encode(encType).decode(encType)
+            lyrics = lyrics.encode(enc_type).decode(enc_type)
 
         except:
             pass
@@ -114,9 +133,8 @@ def get_lyrics(title, artist):
 
 
 def embedd_lyrics(fileName, lyrics):
-    try: 
+    try:
         tags = ID3(fileName)
-            
     except ID3NoHeaderError:
         tags = ID3()
 
@@ -124,19 +142,19 @@ def embedd_lyrics(fileName, lyrics):
         tags.delall(u"USLT::'en'")
         tags.save(fileName)
 
-    #Add lyrics to tag object
-    try:       
+    # Add lyrics to tag object
+    try:
         tags[u"USLT::'eng'"] = (USLT(encoding=3, lang=u'eng', text=lyrics))
-            
+
     except:
-        print("Error inserting lyrics for: " + ntpath.basename(fileName), file = sys.stderr)
+        print("Error inserting lyrics for: " + ntpath.basename(fileName), file=sys.stderr)
         return False
 
     try:
         tags.save(fileName, v2_version=3)
 
-    except Exception as e:
-        print("Error modifying mp3 file", file = sys.stderr)
+    except Exception:
+        print("Error modifying mp3 file", file=sys.stderr)
         return False
 
     return True
@@ -146,26 +164,24 @@ class Window(QWidget):
     def __init__(self, parent=None):
         super(Window, self).__init__(parent)
 
-        self.lyricsDict = {}
-        
         self.songButton = QPushButton("Select Songs")
         self.songButton.clicked.connect(self.songClick)
 
-        #list widget holds all the added song items
-        self.listW = QListWidget()
+        self.lyricsDict = {}
+        self.songList = QListWidget()
 
-        # self.lyricsBox = QLineEdit()
-        # self.lyricsBox.resize(600, 600)
-        # self.lyricsBox.sizePolicy()
-        # self.lyricsBox.setReadOnly(True)
+        self.lyricsBox = QLineEdit()
+        self.lyricsBox.resize(600, 600)
+        self.lyricsBox.sizePolicy()
+        self.lyricsBox.setReadOnly(True)
 
         vBox = QVBoxLayout()
         vBox.addWidget(self.songButton, 0, QtCore.Qt.AlignTop)
-        #vBox.addWidget(self.lyricsBox,0,QtCore.Qt.AlignVCenter)
+        vBox.addWidget(self.lyricsBox, 0, QtCore.Qt.AlignVCenter)
 
         mainLayout = QGridLayout()
-        mainLayout.addLayout(vBox,0,0)
-        mainLayout.addWidget(self.listW,0,1)
+        mainLayout.addLayout(vBox, 0, 0)
+        mainLayout.addWidget(self.songList, 0, 1)
 
         self.setLayout(mainLayout)
 
@@ -177,9 +193,8 @@ class Window(QWidget):
         root.withdraw()
         songFiles = askopenfilenames(filetypes=(("Mp3 files", "*.mp3"),))
 
-        t = threading.Thread(target = self.add_lyrics_to_song, args = (songFiles,))
+        t = threading.Thread(target=self.add_lyrics_to_song, args=(songFiles,))
         t.start()
-
 
     def add_lyrics_to_song(self, songFiles):
         songList = list(songFiles)
@@ -191,35 +206,27 @@ class Window(QWidget):
                 artist = str(sObj["TPE1"])
 
             except:
-                print("Error getting tags from song file: " + ntpath.basename(song), file = sys.stderr)
+                print("Error getting tags from song file: " + ntpath.basename(song), file=sys.stderr)
                 continue
 
             lyrics = get_lyrics(title, artist)
 
-            fail = True
-
             if lyrics:
-                success = embedd_lyrics(song, lyrics)
+                if embedd_lyrics(song, lyrics):
+                    songIdentifier = ''.join([title, " - ", artist])
 
-                if success:
-                    #Add song name to list to show lyrics are embedded.
-                    fullName = ''.join([title, " - ", artist])
-
-                    songListItem = QListWidgetItem(fullName)
+                    songListItem = QListWidgetItem(songIdentifier)
                     songListItem.setTextAlignment(4)
-                    self.listW.addItem(songListItem)
+                    self.songList.addItem(songListItem)
 
-                    self.lyricsDict[fullName] = lyrics
+                    self.lyricsDict[songIdentifier] = lyrics
 
-                    fail = False
-
-            if fail:
-                songListItem = QListWidgetItem(''.join([title, " - ", artist, " Failed"]))
-                songListItem.setTextAlignment(4)
-                self.listW.addItem(songListItem)
+                else:
+                    songListItem = QListWidgetItem(''.join([title, " - ", artist, " Failed"]))
+                    songListItem.setTextAlignment(4)
+                    self.songList.addItem(songListItem)
 
         exit(0)
-
 
 
 if __name__ == '__main__':
